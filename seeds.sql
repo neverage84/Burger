d9a2dae41bca4ce65ad35646e1f0a7b22cbc2b72
Noelle Everage <neverage84@yahoo.com>
2019-12-12 15:56:33 -0800

    Updated syntax for insert to false for seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -1,4 +1,6 @@
 --- Insert a set of records.
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Cheeseburger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Avocado and Egg Cheeseburger", true);
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avocado and Egg Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
