80234d80cb5f2aa34e0b46466a3ede7fa43419aa
Noelle Everage <neverage84@yahoo.com>
2019-12-12 15:49:12 -0800

    Added seeds.sql with 3 entries

---
+++ seeds.sql
@@ -0,0 +1,4 @@
+--- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avocado and Egg Cheeseburger", true);
