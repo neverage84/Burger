520620766d29f6ecb247f0ead94233b70c651e70
Noelle Everage <neverage84@yahoo.com>
2019-12-30 16:38:10 -0600

    created code to call orm functions using burger specific input for the ORM

---
+++ models/burger.js
@@ -0,0 +1,11 @@
+var orm = require("../config/orm.js");
+
+orm.select("*", "burgers");
+
+// Find a pet in the pets table by an animal_name of Rachel.
+orm.create("burgers", "burger_name", submitItem);
+
+// Find the buyer with the most pets.
+orm.update("burgers", devoured:true, id:this);
+
+module.exports = orm;
